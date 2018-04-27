f6ee99cdfcd665be918612f1ff247dea432517c4
Constantine Lebedyuk <constantinelebedyuk@gmail.com>
2018-04-27 23:33:05 +0300

    New commit...

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+
+    $(".search-icon").click(function () {
+        $(".search").fadeIn(300);
+        $(".search-wrapper").addClass("fadeInUp animated");
+    });
+    $(".close, button").click(function () {
+        $(".search").fadeOut(300);
+        $(".search-wrapper").removeClass("fadeInUp animated");
+    });
+
+    $("#top-menu ul li a").click(function () {
+        $("#top-menu ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".toggle_menu").click(function () {
+       $(".burger").toggleClass("burger_active");
+    });
+
+});
