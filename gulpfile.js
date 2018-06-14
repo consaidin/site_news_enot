e32ab037a15ae19ace35796980446e96d33f925a
Constantine Lebedyuk <constantinelebedyuk@gmail.com>
2018-06-14 14:11:49 +0300

    New commit...

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCss = require('gulp-clean-css');
+var gulpIf = require('gulp-if');
+var browserSync = require('browser-sync').create();
 
-const config = {
-    path: {
+var config = {
+    paths: {
         scss: './src/scss/**/*.scss',
         html: './public/index.html'
     },
     output: {
         cssName: 'bundle.min.css',
         path: './public'
-    }
+    },
+    isDevelop: false
 };
 
-gulp.task('scss', function() {
-    return gulp.src(config.path.scss)
-        .pipe(sourcemaps.init())
+gulp.task('scss', function () {
+    return gulp.src(config.paths.scss)
+        .pipe(gulpIf(config.isDevelop, sourcemaps.init()))
         .pipe(sass())
         .pipe(concat(config.output.cssName))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write())
+        .pipe(autoprefixer())
+        .pipe(gulpIf(!config.isDevelop, cleanCss()))
+        .pipe(gulpIf(config.isDevelop, sourcemaps.write()))
         .pipe(gulp.dest(config.output.path))
         .pipe(browserSync.stream());
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', function () {
     browserSync.init({
         server: {
             baseDir: config.output.path
         }
     });
 
-    gulp.watch(config.path.scss, ['scss']);
-    gulp.watch(config.path.html).on('change', browserSync.reload);
-
+    gulp.watch(config.paths.scss, ['scss']);
+    gulp.watch(config.paths.html).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['scss', 'serve']);
